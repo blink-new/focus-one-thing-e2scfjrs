753bd1a7457a9641de8912bc0c53e4b42d841d63
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-03-28 23:09:39 +0000

    Add Dialog Component and Fix Imports

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -7,7 +7,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from './ui/dialog'
+} from '../components/ui/dialog'
 import { Settings as SettingsIcon } from 'lucide-react'
 import { useFocusStore } from '../lib/store'
 import {
